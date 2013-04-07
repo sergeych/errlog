5efa71ef668c1a96340f315d54c2cd76ebf6d2fc
sergeych <real.sergeych@gmail.com>
2013-04-07 11:35:29 +0400

    better behavior if not configured. new report notation ctx.error("test") { |c| c.data = "fubar" } syntax sugar

--- lib/errlog/context.rb
+++ lib/errlog/context.rb
@@ -18,8 +18,7 @@ module Errlog
     end
 
     def exception e, severity=Errlog::ERROR, &block
-      raise unless Errlog.configured?
-      self.stack = e.backtrace
+      self.stack           = e.backtrace
       self.exception_class = e.class.name
       report e.to_s, severity
     end
@@ -36,31 +35,36 @@ module Errlog
 
     def error text, details=nil, severity=Errlog::ERROR, &block
       details and self.details = details
-      report text, severity
+      report text, severity, &block
     end
 
     def before_report &block
       (@before_handlers ||= []) << block
     end
 
-    def report text, severity = Errlog::ERROR, &block
-      raise 'Errlog is not configured. Use Errlog.config' unless Errlog.configured?
-      self.application ||= Errlog.application
-      self.time     = Time.now
-      self.severity = severity
-      self.platform ||= Errlog.default_platform
-      self.stack    ||= caller
-      self.text = text
+    def report text, severity = Errlog::ERROR
+      yield self if block_given?
 
-      if @before_handlers
-        @before_handlers.each { |h|
-          h.call self
-        }
+      unless Errlog.configured?
+        STDERR.puts 'Errlog is not configured. Use Errlog.config'
+      else
+        self.application ||= Errlog.application
+        self.time        = Time.now
+        self.severity    = severity
+        self.platform    ||= Errlog.default_platform
+        self.stack       ||= caller
+        self.text        = text
+
+        if @before_handlers
+          @before_handlers.each { |h|
+            h.call self
+          }
+        end
+
+        @log_records and self.log = @log_records.inject([]) { |all, x| all << x; all }.sort { |x, y| x[1] <=> y[1] }
+        Errlog.rails? and self.rails_root = Rails.root.to_s
+        Errlog.post self.to_hash
       end
-
-      @log_records and self.log = @log_records.inject([]){ |all,x| all << x; all}.sort { |x,y| x[1] <=> y[1] }
-      Errlog.rails? and self.rails_root = Rails.root.to_s
-      Errlog.post(self.to_hash, &block)
     end
 
     MAX_LOG_LINES = 100
--- spec/errlog_spec.rb
+++ spec/errlog_spec.rb
@@ -8,62 +8,78 @@ require 'stringio'
 describe Errlog do
 
   context 'configured' do
-  before :each do
-    Errlog.configure 'TheTestId', '1234567890123456'
-    @packager = Errlog.packager 'TheTestId', '1234567890123456'
-  end
-
-  it 'should have context' do
-    Errlog.context.value = 'test'
-    Errlog.context.value.should == 'test'
-    Errlog.clear_context
-    Errlog.context.value.should == nil
-  end
-
-  # This spec checks the whole chain - protect, exception, report, but does not check post
-  it 'should provide protection' do
-    Errlog.should_receive(:post).exactly(3).times do |payload|
-      payload = Hashie::Mash.new payload
-      payload.should_not be_nil
-      payload.stack[0].should =~ /errlog_spec/
-      payload.text.should == 'TestError'
-      payload.time.should be_within(5000).of(Time.now)
-      payload.component_name.should == 'test'
-      payload.test.should == '123'
+    before :each do
+      Errlog.configure 'TheTestId', '1234567890123456'
+      @packager = Errlog.packager 'TheTestId', '1234567890123456'
     end
 
-    Errlog.protect 'test' do |ctx|
+    it 'should have context' do
+      Errlog.context.value = 'test'
+      Errlog.context.value.should == 'test'
       Errlog.clear_context
-      ctx.test = '123'
-      raise 'TestError'
+      Errlog.context.value.should == nil
     end
 
-    -> {
-      Errlog.protect_rethrow 'test' do |ctx|
+    # This spec checks the whole chain - protect, exception, report, but does not check post
+    it 'should provide protection' do
+      Errlog.should_receive(:post).exactly(3).times do |payload|
+        payload = Hashie::Mash.new payload
+        payload.should_not be_nil
+        payload.stack[0].should =~ /errlog_spec/
+        payload.text.should == 'TestError'
+        payload.time.should be_within(5000).of(Time.now)
+        payload.component_name.should == 'test'
+        payload.test.should == '123'
+      end
+
+      Errlog.protect 'test' do |ctx|
+        Errlog.clear_context
         ctx.test = '123'
         raise 'TestError'
       end
-    }.should raise_error
 
-    Errlog.context.test = '123'
-    Errlog.report 'TestError'
-  end
+      -> {
+        Errlog.protect_rethrow 'test' do |ctx|
+          ctx.test = '123'
+          raise 'TestError'
+        end
+      }.should raise_error
 
-  it 'should provide logs' do
-    Errlog.should_receive(:post).exactly(1).times do |payload|
-      payload = Hashie::Mash.new payload
-      payload.text.should == 'LogTest'
-      payload.log.length.should == 2
-      payload.log[0][3].should == 'test info'
-      payload.log[1][3].should == 'test warning'
+      Errlog.context.test = '123'
+      Errlog.report 'TestError'
     end
-    sio = StringIO.new
-    l1 = Errlog::ChainLogger.new
-    l2 = Errlog::ChainLogger.new Logger.new(sio)
-    l1.info 'test info'
-    l2.warn 'test warning'
-    Errlog.report 'LogTest', Errlog::TRACE
-    sio.string.should match( /W, \[.*\]  WARN -- : test warning/)
+
+    it 'should provide logs' do
+      Errlog.should_receive(:post).exactly(1).times do |payload|
+        payload = Hashie::Mash.new payload
+        payload.text.should == 'LogTest'
+        payload.log.length.should == 2
+        payload.log[0][3].should == 'test info'
+        payload.log[1][3].should == 'test warning'
+      end
+      sio = StringIO.new
+      l1  = Errlog::ChainLogger.new
+      l2  = Errlog::ChainLogger.new Logger.new(sio)
+      l1.info 'test info'
+      l2.warn 'test warning'
+      Errlog.report 'LogTest', Errlog::TRACE
+      sio.string.should match(/W, \[.*\]  WARN -- : test warning/)
+    end
+
+    it 'should provide data in context' do
+      Errlog.clear_context
+      Errlog.should_receive(:post).exactly(1).times do |payload|
+        payload = Hashie::Mash.new payload
+        payload.text.should == 'ParamTest'
+        payload.test1.should == 'test1 data'
+        payload.test2.should == 'test2 data'
+      end
+      Errlog.context.test1 = 'test1 data'
+      Errlog.error "ParamTest" do |c|
+        c.test2 = 'test2 data'
+      end
+    end
+
   end
 
   it 'should provide constants' do
@@ -77,7 +93,7 @@ describe Errlog do
     is_warning?(Errlog::ERROR).should_not be_true
     is_warning?(Errlog::WARNING).should be_true
     is_warning?(Errlog::TRACE).should_not be_true
-    
+
     is_trace?(Errlog::ERROR).should_not be_true
     is_trace?(Errlog::WARNING).should_not be_true
     is_trace?(Errlog::TRACE).should be_true
@@ -87,10 +103,8 @@ describe Errlog do
     Errlog.severity_name(Errlog::TRACE).should == 'trace'
   end
 
-  end
-
   it 'should work fine when not configured' do
-    expect { Errlog.error "Some error" }.to raise_error(StandardError, 'Errlog is not configured. Use Errlog.config')
+    expect { Errlog.error "Some error" }.to_not raise_error
     expect {
       Errlog.protect_rethrow {
         raise "test"
