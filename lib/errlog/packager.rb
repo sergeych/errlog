f9392a0f7915ecc1babb4ae9bd23b1880ff46b7e
sergeych <real.sergeych@gmail.com>
2013-04-16 01:25:16 +0400

    more docs

--- lib/errlog/packager.rb
+++ lib/errlog/packager.rb
@@ -5,15 +5,16 @@ require 'boss-protocol'
 
 module Errlog
 
+  # Packager does (un)packing data to effectively and (where possible) safely
+  # transfer the report over the network. Normally you don't use it directly.
   class Packager
-
-    # class InvalidPackage < Exception; end
 
     def initialize app_id, app_key
       @appid = app_id
       @key = app_key.length == 16 ? app_key : Base64.decode64(app_key)
     end
 
+    # AES-128 encrypt the block
     def encrypt data
       cipher = OpenSSL::Cipher.new('AES-128-CBC')
       cipher.encrypt
@@ -22,6 +23,7 @@ module Errlog
       iv + cipher.update(data) + cipher.update(Digest::MD5.digest(data)) + cipher.final
     end
 
+    # AES-128 decrypt the block
     def decrypt ciphertext
       cipher = OpenSSL::Cipher.new('AES-128-CBC')
       cipher.decrypt
@@ -32,10 +34,13 @@ module Errlog
       digest == Digest::MD5.digest(data) ? data : nil
     end
 
+    # @return [binary] packed payload using the default block format
     def pack payload
       "\x00#{encrypt(Boss.dump @appid, payload)}"
     end
 
+    # @return [Hash] unpacked payload or nil if block fromat is unknown or block seems
+    # to be broken (e.g. wrong credentials)
     def unpack block
       block[0].to_i == 0 or return nil
       id, payload = Boss.load_all(decrypt(block[1..-1]))
